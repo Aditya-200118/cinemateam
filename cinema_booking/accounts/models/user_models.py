05973307d93f338ceab20e67c58915870926d1a2
Aditya Bhargava <bhargavaaditya2001@gmail.com>
2024-12-05 16:02:35 -0500

    set default is_active to false

--- cinema_booking/accounts/models/user_models.py
+++ cinema_booking/accounts/models/user_models.py
@@ -47,7 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = ["first_name", "last_name"]
