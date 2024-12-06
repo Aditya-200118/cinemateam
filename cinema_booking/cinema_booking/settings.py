0652e458b8969ebac9a79c559641a7d7aed70761
Aditya-200118 <65164821+Aditya-200118@users.noreply.github.com>
2024-12-06 15:19:28 -0500

    Updated settings.py to remove credentials

--- cinema_booking/cinema_booking/settings.py
+++ cinema_booking/cinema_booking/settings.py
@@ -27,8 +27,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhargavaaditya172001@gmail.com'
-EMAIL_HOST_PASSWORD = 'cyta twzd sprl ahid'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 # Quick-start development settings - unsuitable for production
